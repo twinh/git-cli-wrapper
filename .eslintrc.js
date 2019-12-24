666d5905b75988e2cb2013f4c802e4bad0b6af34
twinh <twinhuang@qq.com>
2019-12-24 14:36:23 +0800

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: [],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  "env": {
-    "jest": true
+  env: {
+    jest: true
   },
   rules: {
+    'no-restricted-syntax': ['error', 'ForInStatement'], // Allow for of loops @see https://github.com/airbnb/javascript/issues/1122
     'object-curly-spacing': ['error', 'never'],
     '@typescript-eslint/explicit-function-return-type': ['off'], // Conflict with no-useless-return
-    '@typescript-eslint/no-explicit-any': ["error", {ignoreRestArgs: true}]
+    '@typescript-eslint/no-explicit-any': ["error", {ignoreRestArgs: true}],
   },
 };
